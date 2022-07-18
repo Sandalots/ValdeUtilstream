8ca6787c82db249ef13ebdbd6a151a1eae879038
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-07-18 07:40:26 +0100

    Update Starstruck.py

--- Starstruck.py
+++ Starstruck.py
@@ -2,12 +2,9 @@ from art import *
 
 def printStar():
     tprint("Starstruck!", font="random")
-
-def testPrint():
-    assert printStar() == printStar()
+    
 
 if __name__ == "__main__":
     printStar()
-    testPrint()
 else:
     pass
