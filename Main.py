002f303c6ac81a9df67acc1f877a002cbde0f75c
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-09-15 14:49:19 +0100

    sturct

--- Main.py
+++ Main.py
@@ -1,17 +1,11 @@
 import Starstruck
-import rStr
 class Main:
     def __init__(self):
         self.starstruck = Starstruck.Starstruck()
 
     def run(self):
         self.starstruck.run()
-    
-    def randomise(self):
-        self.starstruck.stringer = rStr.randomString()
-        self.starstruck.run()
 
 if __name__ == "__main__":
     main = Main()
     main.run()
-    main.randomise()
--- Starstruck.py
+++ Starstruck.py
@@ -6,7 +6,7 @@ def stringer():
 
 def printStar():
     return tprint(stringer(), font="random")
-
+    
 class Starstruck:
     def __init__(self):
        pass
